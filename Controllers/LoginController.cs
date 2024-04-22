05755f74a44275a438472a38472f9149e5a7fb03
Mehrab-HossainCSE <mehrab15-13769@diu.edu.bd>
2024-04-22 23:44:28 +0600

    first commit

---
+++ Controllers/LoginController.cs
@@ -0,0 +1,66 @@
+﻿using System.IdentityModel.Tokens.Jwt;
+using System.Text;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
+
+namespace JWT_Token.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class LoginController : ControllerBase
+    {
+        private IConfiguration _config;
+
+        public LoginController(IConfiguration configuration)
+        {
+            _config = configuration;
+            
+        }
+        private Model.User AuthenticateUser(Model.User user)
+        {
+            Model.User _user = null;
+            if(user.Username=="Admin" && user.Password == "12345")
+            {
+                _user = new Model.User { Username = "Mehrab" };
+            }
+            return _user;
+        }
+        private string GenerateToken(Model.User user)
+        {
+            var securityKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_config["Jwt:Key"]));
+            var credentials = new SigningCredentials(securityKey, SecurityAlgorithms.HmacSha256);
+
+            // Create token
+            var token = new JwtSecurityToken(
+                issuer: _config["Jwt:Issuer"],
+                audience: _config["Jwt:Audience"],
+                claims: null,
+                expires: DateTime.Now.AddMinutes(1), // Token expiration time
+                signingCredentials: credentials
+            );
+
+            // Serialize token to string
+            return new JwtSecurityTokenHandler().WriteToken(token);
+
+        }
+
+        [AllowAnonymous]
+        [HttpPost]
+        public IActionResult Login(Model.User user)
+        {
+            IActionResult response =Unauthorized();
+            var user_ = AuthenticateUser(user);
+            if (user_ != null)
+            {
+                var token = GenerateToken(user_);
+                response = Ok(new { token = token });
+            }
+            return response;
+
+        }
+    }
+
+    
+}
